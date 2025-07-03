ba6ac8502d940ef5d027dd35bf66570184c2d0df
patient <undefinedpatient@gmail.com>
2025-07-03 14:33:42 +0800

    Updated basic Form Field && limit window size

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:idea_cache/app.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+  await windowManager.setMinimumSize(const Size(720, 420));
   runApp(const ICApp());
 }
